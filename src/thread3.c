19adb33bc3e93db6d09e57688cd47687e6345ece
Ryan Myers <ryan.p.myers@gmail.com>
2024-05-10 16:03:04 -0400

    Make a buildable C file for thread3

---
+++ src/thread3.c
@@ -0,0 +1,64 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80000920.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_8000092C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/unref_80000938.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80000970.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_800009D8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/unref_80000A34.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80000B54.s")
+
+void __dummy_80000C5C(void) {
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80000C64.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80000CF4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80000D44.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80000DD4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80000E24.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80000E5C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80000EAC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80000F30.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_800016D8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80001764.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_800017B8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_800018E0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80001968.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80001A00.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80001E64.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80001FF4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_8000205C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_800020D0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_80002340.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_800024EC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/thread3_scheduler.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/func_800029D8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread3/osViExtendVStart.s")

2544c9ad0473fcc02b42c706075289493e285203
Ryan Myers <ryan.p.myers@gmail.com>
2024-05-14 14:42:30 -0400

    Create thread6 c file

---
+++ src/thread6.c
@@ -0,0 +1,35 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/func_80003C00.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/update_controller_status.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/read_controller_data.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/update_global_contdata.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/initialize_controllers.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/dispatch_contevt.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/schedule_contread.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/update_contdata.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/unref_80004338.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/enable_auto_contread.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/set_contstatus_delay.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/func_800043C0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/func_80004474.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/func_80004494.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/func_800044B4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/handle_contevt.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/thread6/thread6_controllers.s")

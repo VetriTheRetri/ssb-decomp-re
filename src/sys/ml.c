f1626c92769d7e7a920b92c93c5997e3dfec7215
Ryan Myers <ryan.p.myers@gmail.com>
2024-05-16 09:38:24 -0400

    Migrate ml.c

---
+++ src/sys/ml.c
@@ -0,0 +1,42 @@
+#include "sys/ml.h"
+
+#include "sys/crash.h"
+
+#include <ssb_types.h>
+
+#include <PR/ultratypes.h>
+
+void mlResetBumpAlloc(mlBumpAllocRegion *bp) 
+{
+    bp->ptr = bp->start;
+}
+
+void* mlSetBumpAlloc(mlBumpAllocRegion *bp, u32 size, u32 alignment) 
+{
+    u8 *aligned;
+    u32 offset;
+
+    if (alignment != 0) 
+    {
+        offset  = alignment - 1;
+        aligned = (u8*)(((uintptr_t)bp->ptr + (offset)) & ~(offset));
+    } 
+    else aligned = (u8*)bp->ptr;
+
+    bp->ptr = (void*)(aligned + size);
+
+    if (bp->end < bp->ptr) 
+    {
+        gsFatalPrintF("ml : alloc overflow #%d\n", bp->id);
+        while (TRUE); // { }
+    }
+    return (void*)aligned;
+}
+
+void mlInitBumpAlloc(mlBumpAllocRegion *bp, u32 id, void *start, u32 size) 
+{
+    bp->id    = id;
+    bp->ptr   = start;
+    bp->start = start;
+    bp->end   = (void*) ((uintptr_t)start + size);
+}

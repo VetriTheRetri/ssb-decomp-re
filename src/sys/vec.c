b31314692cc303851092443bd8114f37b1f210a6
Ryan Myers <ryan.p.myers@gmail.com>
2024-05-17 10:22:22 -0400

    Update vec func names, and create it's C file

---
+++ src/sys/vec.c
@@ -0,0 +1,47 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fNormalize.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fMagnitude.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fAddTo.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fSubtractFrom.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fSubtract.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fSubtractNormalize.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fScale.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fDivideScale.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fAddScaled.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fScaleAddScaledNormalize.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fAngleDiff.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fGetEulerRotation.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fNegate.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/func_80019438.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fNegateByAxis.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/unref_80019608.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/unref_80019818.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/unref_80019888.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fCompareDirection.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fNormalizedCross.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fSubtractCompareDirection.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/lbVector_Vec3fDiffMagnitude.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/sys/vec/unref_80019B00.s")

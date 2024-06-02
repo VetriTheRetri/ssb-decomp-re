cf06ab0b917226ce3dba4b544c34546a2a0bf23a
Ryan Myers <ryan.p.myers@gmail.com>
2024-06-01 22:10:51 -0400

    seq

---
+++ src/libultra/n_audio/seq.c
@@ -0,0 +1,22 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/n_audio/seq/alSeqSetLoc.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/n_audio/seq/alSeqGetTicks.s")
+
+void func_8002F704_30304(void) {
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/n_audio/seq/readVarLen.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/n_audio/seq/alSeqNextEvent.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/n_audio/seq/alSeqNewMarker.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/n_audio/seq/__alSeqNextDelta.s")
+
+void func_8002FA54(void) {
+}
+
+void func_8002FA5C(void) {
+}

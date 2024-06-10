cdc8862f1aa1b9966b9fcd40199f5d5313df3137
Santiago <san@san.com>
2024-06-10 23:42:52 +0200

    update rename script

--- tools/renameSymbol.py
+++ tools/renameSymbol.py
@@ -6,17 +6,37 @@ import sys
 def filesInFolderRec(folder):
 	return [os.path.join(dp, f).replace('\\', '/') for dp, dn, filenames in os.walk(folder) for f in filenames]
 
-if __name__ == "__main__":
-	if len(sys.argv) != 3:
-		print("Usage:")
-		print("    renameSymbol <oldSymbolName> <newSymbolName>")
-		sys.exit(1)
-
-	fileList = filesInFolderRec('symbols')
-	fileList.extend(filesInFolderRec('include'))
-	fileList.extend(filesInFolderRec('src'))
+def renameSymbol(oldSymbolName, newSymbolName, baseDir='./', force=False):
+	fileList = filesInFolderRec(f'{baseDir}symbols')
+	fileList.extend(filesInFolderRec(f'{baseDir}include'))
+	fileList.extend(filesInFolderRec(f'{baseDir}src'))
+	if not force:
+		for filePath in fileList:
+			with open(filePath, 'r') as file:
+				source = file.read()
+			if newSymbolName in source:
+				print(f"Not renaming to prevent symbol collision")
+				return False
 	for filePath in fileList:
 		with open(filePath, 'r') as file:
 			source = file.read()
 		with open(filePath, 'w') as file:
-			file.write(source.replace(sys.argv[1], sys.argv[2]))
+			file.write(source.replace(oldSymbolName, newSymbolName))
+	return True
+
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print("Usage:")
+		print("    renameSymbol <oldSymbolName> <newSymbolName> [--force]")
+		print('    renameSymbol "<oldSymbolNameA>=<newSymbolNameA>;<oldSymbolNameB>=<newSymbolNameB>;..." [--force]')
+		sys.exit(1)
+	force = "--force" in sys.argv
+	if force:
+		sys.argv.remove("--force")
+	if '=' in sys.argv[1]:
+		for replacement in sys.argv[1].split(';'):
+			oldSymbolName, newSymbolName = replacement.split('=')
+			renameSymbol(oldSymbolName, newSymbolName, './', force)
+	else:
+		renameSymbol(sys.argv[1], sys.argv[2], './', force)

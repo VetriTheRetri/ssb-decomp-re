d8d0c6a5db4df8c42437b8d548d9550284d8ea8a
Santiago <san@san.com>
2024-06-15 10:01:01 +0200

    update renameSymbol script to be more accurate

--- tools/renameSymbol.py
+++ tools/renameSymbol.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os
+import re
 import sys
 
 def filesInFolderRec(folder):
@@ -12,16 +13,31 @@ def renameSymbol(oldSymbolName, newSymbolName, baseDir='./', force=False):
 	fileList.extend(filesInFolderRec(f'{baseDir}src'))
 	if not force:
 		for filePath in fileList:
-			with open(filePath, 'r') as file:
-				source = file.read()
-			if newSymbolName in source:
-				print(f"Not renaming {oldSymbolName} to prevent symbol collision ({newSymbolName} already exists in the codebase)")
-				return False
+			with open(filePath, 'r') as sourceFile:
+				source = sourceFile.read()
+			sourceLines = []
+			for line in source.split('\n'):
+				if newSymbolName in [line[x.start():x.end()] for x in re.finditer(r"\w+", line)]:
+					print(f"Not renaming {oldSymbolName} to prevent symbol collision ({newSymbolName} already exists in the codebase)")
+					return False
+
+	print(f"Renaming {oldSymbolName} to {newSymbolName}")
 	for filePath in fileList:
-		with open(filePath, 'r') as file:
-			source = file.read()
-		with open(filePath, 'w') as file:
-			file.write(source.replace(oldSymbolName, newSymbolName))
+		with open(filePath, 'r') as sourceFile:
+			source = sourceFile.read()
+		sourceLines = []
+		for line in source.split('\n'):
+			wordMatches = [(x.start(), x.end()) for x in re.finditer(r"\w+", line)]
+			for wm in reversed(wordMatches):
+				wms = wm[0]
+				wme = wm[1]
+				wmv = line[wms:wme]
+				if wmv == oldSymbolName:
+					line = line[:wms] + newSymbolName + line[wme:]
+			sourceLines.append(line)
+
+		with open(filePath, 'w') as sourceFile:
+			sourceFile.write("\n".join(sourceLines))
 	return True
 
 

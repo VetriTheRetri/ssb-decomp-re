989d22e28cd03bfef5a960d68fe6c0ad8eb36661
VetriTheRetri <jupiter9764@gmail.com>
2024-06-18 21:48:56 +0300

    add ftbossyupideppou3.c

---
+++ src/ft/ftchar/ftboss/ftbossyubideppou3.c
@@ -0,0 +1,78 @@
+#include <ft/fighter.h>
+
+// // // // // // // // // // // //
+//                               //
+//           FUNCTIONS           //
+//                               //
+// // // // // // // // // // // //
+
+// 0x8015A3C0
+void ftBossYubideppou3ProcUpdate(GObj *fighter_gobj)
+{
+    ftStruct *fp = ftGetStruct(fighter_gobj);
+
+    if ((fp->fighter_vars.boss.p->wait_div == 1.5F) && (fighter_gobj->anim_frame >= 20.0F) && (fp->status_vars.boss.yubideppou.bullet_count != 3))
+    {
+        ftBossYubideppou3SetStatus(fighter_gobj);
+    }
+    else if (fighter_gobj->anim_frame <= 0.0F)
+    {
+        ftBossWaitSetStatus(fighter_gobj);
+    }
+}
+
+// 0x8015A45C
+void ftBossYubideppou3ProcPhysics(GObj *fighter_gobj)
+{
+    ftStruct *fp = ftGetStruct(fighter_gobj);
+    Vec3f pos;
+
+    if (fp->status_vars.boss.yubideppou.shoot_timer != 0)
+    {
+        fp->status_vars.boss.yubideppou.shoot_timer--;
+
+        if (fp->status_vars.boss.yubideppou.shoot_timer == 0)
+        {
+            pos.x = pos.y = pos.z = 0.0F;
+
+            ftParts_GetDObjWorldPosition(fp->joint[19], &pos);
+
+            if ((fp->fighter_vars.boss.p->wait_div == 1.5F) && (fp->status_vars.boss.yubideppou.bullet_count != 3))
+            {
+                wpBossBulletHardMakeWeapon(fighter_gobj, &pos);
+            }
+            else wpBossBulletNormalMakeWeapon(fighter_gobj, &pos); 
+        }
+    }
+    if (fp->fighter_vars.boss.p->wait_div == 1.5F)
+    {
+        ftBossYubideppou2UpdatePosition(fighter_gobj);
+    }
+}
+
+// 0x8015A538
+void ftBossYubideppou3SetStatus(GObj *fighter_gobj)
+{
+    ftStruct *fp;
+    Vec3f pos;
+
+    ftMainSetFighterStatus(fighter_gobj, ftStatus_Boss_Yubideppou3, 0.0F, 1.0F, FTSTATUPDATE_NONE_PRESERVE);
+
+    fp = ftGetStruct(fighter_gobj);
+
+    fp->status_vars.boss.yubideppou.bullet_count++;
+
+    pos.x = 0.0F;
+    pos.y = 0.0F;
+    pos.z = 0.0F;
+
+    ftParts_GetDObjWorldPosition(fp->joint[15], &pos);
+
+    if ((fp->fighter_vars.boss.p->wait_div == 1.5F) && (fp->status_vars.boss.yubideppou.bullet_count != 3))
+    {
+        wpBossBulletHardMakeWeapon(fighter_gobj, &pos);
+    }
+    else wpBossBulletNormalMakeWeapon(fighter_gobj, &pos);
+    
+    fp->status_vars.boss.yubideppou.shoot_timer = 4;
+}

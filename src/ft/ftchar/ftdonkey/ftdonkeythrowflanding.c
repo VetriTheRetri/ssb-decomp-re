25e436240c32c64f7e8fba22202f68468d4ee89a
VetriTheRetri <jupiter9764@gmail.com>
2024-06-20 09:33:10 +0300

    move ftdonkeythrowflanding to correct folder

--- src/ft/ftchar/ftdonkey/ftdonkeythrowflanding.c
+++ src/ft/ftchar/ftdonkey/ftdonkeythrowflanding.c
@@ -1,5 +1,31 @@
-#include "common.h"
+#include <ft/fighter.h>
 
-#pragma GLOBAL_ASM("asm/nonmatchings/ft/ftchar/ftdonkey/ftdonkeythrowflanding/ftDonkeyThrowFLandingProcUpdate.s")
+// // // // // // // // // // // //
+//                               //
+//           FUNCTIONS           //
+//                               //
+// // // // // // // // // // // //
 
-#pragma GLOBAL_ASM("asm/nonmatchings/ft/ftchar/ftdonkey/ftdonkeythrowflanding/ftDonkeyThrowFLandingSetStatus.s")
+// 0x8014DC50
+void ftDonkeyThrowFLandingProcUpdate(GObj *fighter_gobj)
+{
+    ftStruct *fp = ftGetStruct(fighter_gobj);
+
+    fp->status_vars.common.throwf.landing_anim_frame++;
+
+    if (fp->status_vars.common.throwf.landing_anim_frame <= 4.0F)
+    {
+        ftDonkeyThrowFWaitSetStatus(fighter_gobj);
+    }
+}
+
+// 0x8014DCA4
+void ftDonkeyThrowFLandingSetStatus(GObj *fighter_gobj)
+{
+    ftStruct *fp = ftGetStruct(fighter_gobj);
+
+    ftMap_SetGround(fp);
+    ftMainSetFighterStatus(fighter_gobj, ftStatus_Donkey_ThrowFLanding, 0.0F, 0.0F, FTSTATUPDATE_NONE_PRESERVE);
+
+    fp->status_vars.common.throwf.landing_anim_frame = 0.0F;
+}

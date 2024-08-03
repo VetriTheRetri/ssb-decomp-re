7c2630246c9dbe1e301ff5e447c76c1827f2e8df
Santiago <san@san>
2024-08-02 22:31:08 -0400

    use curl instead of wget for compatibility and better ido check

--- installDependencies.sh
+++ installDependencies.sh
@@ -43,16 +43,16 @@ case "${unameOut}" in
 esac
 RECOMP_DIR7=tools/ido-recomp/7.1
 RECOMP_DIR5=tools/ido-recomp/5.3
-if [ ! -d "${RECOMP_DIR7}" ]
+if [ ! -f "${RECOMP_DIR7}/cc" ]
 then
-	wget https://github.com/decompals/ido-static-recomp/releases/download/v1.1/ido-7.1-recomp-${DETECTED_OS}.tar.gz
+	curl https://github.com/decompals/ido-static-recomp/releases/download/v1.1/ido-7.1-recomp-${DETECTED_OS}.tar.gz -O ido-7.1-recomp-${DETECTED_OS}.tar.gz -L
 	mkdir -p ${RECOMP_DIR7}
 	tar xf ido-7.1-recomp-${DETECTED_OS}.tar.gz -C ${RECOMP_DIR7}
 	rm ido-7.1-recomp-${DETECTED_OS}.tar.gz
 fi
-if [ ! -d "${RECOMP_DIR5}" ]
+if [ ! -f "${RECOMP_DIR5}/cc" ]
 then
-	wget https://github.com/decompals/ido-static-recomp/releases/download/v1.1/ido-5.3-recomp-${DETECTED_OS}.tar.gz
+	curl https://github.com/decompals/ido-static-recomp/releases/download/v1.1/ido-5.3-recomp-${DETECTED_OS}.tar.gz -O ido-5.3-recomp-${DETECTED_OS}.tar.gz -L
 	mkdir -p ${RECOMP_DIR5}
 	tar xf ido-5.3-recomp-${DETECTED_OS}.tar.gz -C ${RECOMP_DIR5}
 	rm ido-5.3-recomp-${DETECTED_OS}.tar.gz

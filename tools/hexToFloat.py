2eebb7b8109d837b30f31d7119401ad8486e8f9c
Santiago <san@san.com>
2024-08-24 01:11:01 +0200

    add hex to float script

---
+++ tools/hexToFloat.py
@@ -0,0 +1,9 @@
+import sys
+import struct
+
+if len(sys.argv) < 2:
+	printf("Usage: hexToFloat <floatInHex> [<moreFloatsInHex>...]")
+	sys.exit(1)
+
+for arg in sys.argv[1:]:
+	print(struct.unpack('!f', bytes.fromhex(arg if not arg.startswith("0x") else arg[2:]))[0])

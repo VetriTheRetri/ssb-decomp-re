eab3c39d56fef352691adc44a88c323ac69f343d
John Liljegren <jtl3d@users.noreply.github.com>
2025-01-24 16:58:55 -0500

    some ovl8 matches

--- src/ovl8/ovl8_29.c
+++ src/ovl8/ovl8_29.c
@@ -1,5 +1,6 @@
 #include "common.h"
 
+// 0x80385A50
 s32 func_ovl8_80385A50(s32 arg0, s32 arg1)
 {
 	s32 sp24;
@@ -12,18 +13,28 @@ s32 func_ovl8_80385A50(s32 arg0, s32 arg1)
 	}
 	else
 		sp24 = 0;
-	
+
 	return sp24;
 }
 
+// 0x80385AAC
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_29/func_ovl8_80385AAC.s")
 
+// 0x80385B74
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_29/func_ovl8_80385B74.s")
 
+// 0x80385C70
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_29/func_ovl8_80385C70.s")
 
+// 0x80385D34
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_29/func_ovl8_80385D34.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_29/func_ovl8_80385D54.s")
+// 0x80385D54
+void func_ovl8_80385D54(s32 arg0, s32 arg1)
+{
+    func_ovl8_803817C0(arg0 + 0xB8);
+    func_ovl8_80373694(arg0, arg1);
+}
 
+// 0x80385D88
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_29/func_ovl8_80385D88.s")

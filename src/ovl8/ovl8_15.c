7d81ac84ae31396e7cffc706966f9b077538361a
John Liljegren <jtl3d@users.noreply.github.com>
2025-02-05 21:23:45 -0500

    some ovl8 matches

--- src/ovl8/ovl8_15.c
+++ src/ovl8/ovl8_15.c
@@ -1,10 +1,60 @@
 #include "common.h"
+#include <sys/develop.h>
 
+extern dbUnknownLinkStruct D_ovl8_8038AF38;
+extern dbFunction D_ovl8_8038B018;
+extern dbUnknownLink D_ovl8_8038B170;
+
+// 0x8037F300
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_15/func_ovl8_8037F300.s")
+// void* func_ovl8_8037F300(dbUnknown5* arg0, dbUnknownLinkStruct* arg1, dbUnknownLink* arg2)
+// {
+//     if (arg0 != NULL || ((arg0 = func_ovl8_803717A0(0xB4)) != NULL))
+//     {
+//         if (arg1 == NULL)
+//         {
+//             arg1 = &arg0->unk_dbunk5_0x4C;
+//             arg2 = &arg0->unk_dbunk5_0xA8;
+//             func_ovl8_803717E0(arg1);
+//             func_ovl8_8037C2D0(arg2);
+//         }
 
+//         func_ovl8_803733AC(arg0, arg1, arg2);
+
+//         arg0->unk_dbunk5_0x30 = &D_ovl8_8038AF38;
+//         arg1->db_func = &D_ovl8_8038B018;
+//         arg2->unk_dbunklink_0x8 = &D_ovl8_8038B170;
+
+//     }
+
+//     return arg0;
+// }
+
+// 0x8037F3A8
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_15/func_ovl8_8037F3A8.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_15/func_ovl8_8037F4F4.s")
+// 0x8037F4F4
+void func_ovl8_8037F4F4(dbUnknown5* arg0, s32 arg1)
+{
+    if (arg0 != NULL)
+    {
+        arg0->unk_dbunk5_0x30 = &D_ovl8_8038AF38,
+        arg0->unk_dbunk5_0x38->db_func = &D_ovl8_8038B018;
+        arg0->unk_dbunk5_0x34->unk_dbunklink_0x8 = &D_ovl8_8038B170;
+
+        func_ovl8_8037354C(arg0, 0);
+
+        if (arg1 != 0)
+        {
+            func_ovl8_8037C30C(arg0->unk_dbunk5_0x34, 0);
+            func_ovl8_803718C4(arg0->unk_dbunk5_0x38, 0);
+        }
+        if (arg1 & 1)
+        {
+            func_ovl8_803717C0(arg0);
+        }
+    }
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_15/func_ovl8_8037F590.s")
 

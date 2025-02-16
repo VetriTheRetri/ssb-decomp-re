250062a0635f3d0c14a93d9712e39a4c5efb36c9
John Liljegren <jtl3d@users.noreply.github.com>
2025-02-16 12:31:15 -0500

    some ovl8 matches

--- src/ovl8/ovl8_31.c
+++ src/ovl8/ovl8_31.c
@@ -10,6 +10,11 @@ extern s32 D_ovl8_80388298;
 extern void* func_ovl8_803867E8;
 extern void* func_ovl8_80386994;
 
+extern dbFunction D_ovl8_8038E5A0;
+extern dbFunction D_ovl8_8038E660;
+extern dbFunction D_ovl8_8038E7B8;
+extern s32 D_ovl8_8038E7E0;
+
 void func_ovl8_80381B70(s32, s32*);
 s32 func_ovl8_803865D0(DBMenuPosition*, s32, char*);
 
@@ -62,18 +67,107 @@ s32 func_ovl8_803865D0(DBMenuPosition* arg0, s32 arg1, char* arg2)
     return D_803903D4_1ACC24->unk_dbunk5_0xA8;
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_31/func_ovl8_803866AC.s")
+// 0x803866AC
+s32 func_ovl8_803866AC(dbUnknown5* arg0, s32 arg1, dbUnknown5* arg2)
+{
+    dbFunction* temp_v0;
+    dbUnknownS38* temp_v1;
 
+    if (arg1 == -1) goto bn1;
+    else if (arg1 == 129) goto b129;
+    else if (arg1 == 132) goto b132;
+    else if (arg1 == 128) goto b128;
+    else if (arg1 == 130) goto b130;
+    else if (arg1 != 131) goto end;
+    else goto b131;
+
+    bn1:
+        func_ovl8_8037486C(arg2);
+        goto end;
+
+    b129:
+        if (TRUE);
+        arg0->unk_dbunk5_0xA8 = 1;
+        D_803903D0_1ACC20 = 1;
+        goto end;
+
+    b132:
+        if (TRUE);
+        D_803903D0_1ACC20 = 1;
+        arg0->unk_dbunk5_0xA8 = 0;
+        goto end;
+
+    b128:
+        temp_v0 = arg0->unk_dbunk5_0xAC->unk_dbunks38_0x30;
+        temp_v1 = arg0->unk_dbunk5_0xAC;
+
+        temp_v0[27].unk_dbfunc_0x4(temp_v0[27].unk_dbfunc_0x0 + (uintptr_t)temp_v1, (u8)arg2->unk_dbunk5_0xC);
+        temp_v1 = arg0->unk_dbunk5_0xAC;
+        temp_v0 = temp_v1->unk_dbunks38_0x30;
+        temp_v0[28].unk_dbfunc_0x4(temp_v0[28].unk_dbfunc_0x0 + (uintptr_t)temp_v1);
+        goto end;
+
+    b130:
+        temp_v1 = arg0->unk_dbunk5_0xAC;
+        temp_v0 = temp_v1->unk_dbunks38_0x30;
+        temp_v0[26].unk_dbfunc_0x4(temp_v0[26].unk_dbfunc_0x0 + (uintptr_t)temp_v1, 0x20);
+        temp_v1 = arg0->unk_dbunk5_0xAC;
+        temp_v0 = temp_v1->unk_dbunks38_0x30;
+        temp_v0[28].unk_dbfunc_0x4(temp_v0[28].unk_dbfunc_0x0 + (uintptr_t)temp_v1);
+        goto end;
+
+    b131:
+        temp_v1 = arg0->unk_dbunk5_0xAC;
+        temp_v0 = temp_v1->unk_dbunks38_0x30;
+        temp_v0[32].unk_dbfunc_0x4(temp_v0[32].unk_dbfunc_0x0 + (uintptr_t)temp_v1);
+        goto end;
+
+    end:
+    return 0;
+}
+
+// 0x803867E8
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_31/func_ovl8_803867E8.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_31/func_ovl8_803868C0.s")
+// 0x803868C0
+void func_ovl8_803868C0(dbUnknown5* arg0)
+{
+    arg0->unk_dbunk5_0xAC = func_ovl8_8037319C(arg0, 1);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_31/func_ovl8_803868EC.s")
+// void func_ovl8_803868EC(dbUnknownS38* arg0, s32 arg1)
+// {
+//     if (arg0 != NULL)
+//     {
+//         arg0->unk_dbunks38_0x18 = &D_ovl8_8038E5A0,
+//         arg0->unk_dbunks38_0x20->db_func = &D_ovl8_8038E660;
+//         arg0->unk_dbunks38_0x1C->unk_dbunklink_0x8 = &D_ovl8_8038E7B8;
+//         arg0->unk_dbunks38_0x38.unk_dbunkstruct_0xC = &D_ovl8_8038E7E0;
+//         // arg0->unk_dbunks38_0x38.unk_dbunkstruct_0xC = D_ovl8_8038E7E0;
 
+//         func_ovl8_803761F4(arg0, 0);
+
+//         if (arg1 != 0)
+//         {
+//             func_ovl8_8037C30C(arg0->unk_dbunks38_0x1C, 0);
+//             func_ovl8_803718C4(arg0->unk_dbunks38_0x20, 0);
+//         }
+
+//         if (arg1 & 1)
+//         {
+//             func_ovl8_803717C0(arg0);
+//         }
+//     }
+// }
+
+// 0x80386994
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_31/func_ovl8_80386994.s")
 
+// 0x80386AFC
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_31/func_ovl8_80386AFC.s")
 
+// 0x80386BE0
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_31/func_ovl8_80386BE0.s")
 
 // 0x80386F90
@@ -91,8 +185,10 @@ int stringToNumber(char *string)
 	return value;
 }
 
+// 0x80386FE0
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_31/func_ovl8_80386FE0.s")
 
+// 0x80387154
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_31/func_ovl8_80387154.s")
 
 // 0x80387420
@@ -144,5 +240,5 @@ u32 stringFromNumber(char* string, s64 number)
 	return charsWritten;
 }
 
-
+// 0x80387540
 #pragma GLOBAL_ASM("asm/nonmatchings/ovl8/ovl8_31/func_ovl8_80387540.s")
